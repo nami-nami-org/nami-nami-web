f4f7bda6f9f5acd852a6140ce855e3fb1639deb3
Haui <luigfmp@gmail.com>
2025-09-17 08:59:15 -0500

    feat: :tada: add commitlint configuration with emoji validation in subject

---
+++ commitlint.config.ts
@@ -0,0 +1,26 @@
+/** @type {import('@commitlint/types').UserConfig} */
+
+const config = {
+  extends: ['@commitlint/config-conventional'],
+  parserPreset: 'conventional-changelog-atom',
+  formatter: '@commitlint/format',
+  helpUrl: 'https://github.com/conventional-changelog/commitlint/#what-is-commitlint',
+  rules: {
+    'type-empty': [2, 'never'],
+    'subject-empty': [2, 'never'],
+    'emoji-in-subject': [2, 'always']
+  },
+
+  plugins: [
+    {
+      rules: {
+        'emoji-in-subject': ({ header }: { header: string }) => {
+          const valid = /^.+:\s((:[a-z0-9_+:-]+:)|(\p{Emoji}))/u.test(header)
+          return [valid, '❌ Falta gitmoji después del subject. Ej: "feat: :fire: commit" o "fix: 🐛 commit"']
+        }
+      }
+    }
+  ]
+}
+
+export default config
