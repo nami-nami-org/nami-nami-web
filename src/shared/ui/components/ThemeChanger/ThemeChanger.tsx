d610809a517f1a30d5c8d1606034f95807f37582
Haui <luigfmp@gmail.com>
2025-09-18 06:44:24 -0500

    feat: :sparkles: implement theme selection menu using Headless UI for improved user experience

--- src/shared/ui/components/ThemeChanger/ThemeChanger.tsx
+++ src/shared/ui/components/ThemeChanger/ThemeChanger.tsx
@@ -1,36 +1,50 @@
 'use client'
 
+import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
 import { LaptopIcon, MoonStarIcon, SunDimIcon } from 'lucide-react'
 import { useTheme } from 'next-themes'
+import { useState } from 'react'
+
+import Button from '../Button'
+
+type Theme = {
+  id: string
+  name: string
+  icon: React.ComponentType<{ className?: string }>
+}
+
+const themes: Theme[] = [
+  { id: 'light', name: 'Claro', icon: SunDimIcon },
+  { id: 'dark', name: 'Oscuro', icon: MoonStarIcon },
+  { id: 'system', name: 'Sistema', icon: LaptopIcon }
+]
 
 const ThemeChanger = () => {
   const { theme, setTheme } = useTheme()
+  const [selectedTheme, setSelectedTheme] = useState<Theme>(themes.find(t => t.id === theme) || themes[1])
+
+  const handleThemeChange = (newTheme: Theme) => {
+    setSelectedTheme(newTheme)
+    setTheme(newTheme.id)
+  }
 
   return (
-    <div className='bg-bg2 flex items-center space-x-1 rounded-md p-1.5 shadow-sm'>
-      <button
-        onClick={() => setTheme('light')}
-        className={`rounded-md p-1.5 transition-colors duration-300 ${theme === 'light' ? 'bg-bg3' : 'bg-transparent'}`}
-        aria-label='Cambiar a modo claro'
-      >
-        <SunDimIcon className='text-fn1' />
-      </button>
-
-      <button
-        onClick={() => setTheme('dark')}
-        className={`rounded-md p-1.5 transition-colors duration-300 ${theme === 'dark' ? 'bg-bg3' : 'bg-transparent'}`}
-        aria-label='Cambiar a modo oscuro'
-      >
-        <MoonStarIcon className='text-fn1' />
-      </button>
-
-      <button
-        onClick={() => setTheme('system')}
-        className={`rounded-md p-1.5 transition-colors duration-300 ${theme === 'system' ? 'bg-bg3' : 'bg-transparent'}`}
-        aria-label='Cambiar a modo del sistema'
-      >
-        <LaptopIcon className='text-fn1' />
-      </button>
+    <div className='w-fit text-right'>
+      <Menu>
+        <MenuButton as={Button} className='focus:outline-none'>
+          <selectedTheme.icon className='text-fn1 h-5 w-5' />
+        </MenuButton>
+        <MenuItems anchor='bottom end' className='bg-bg2 w-32 origin-bottom-right rounded-md p-1 shadow-lg focus:outline-none'>
+          {themes.map(t => (
+            <MenuItem key={t.id}>
+              <Button onClick={() => handleThemeChange(t)} className={`w-full ${selectedTheme.id === t.id ? 'bg-bg3' : ''}`}>
+                <t.icon className='text-fn1 h-4 w-4' />
+                <h4 className='text-fn2'>{t.name}</h4>
+              </Button>
+            </MenuItem>
+          ))}
+        </MenuItems>
+      </Menu>
     </div>
   )
 }
