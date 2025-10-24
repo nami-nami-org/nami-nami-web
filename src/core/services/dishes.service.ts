499da0e1773becbc6164bc6041d06a94a37653ff
Jefferson <jgonzalezchaca@gmail.com>
2025-10-24 01:25:55 -0500

    fix: :bug: corregido el nombre del servicio de platos y actualizado el import de RestaurantCard

--- src/core/services/dishes.service.ts
+++ src/core/services/dishes.service.ts
@@ -2,25 +2,17 @@ import nami_api from './root.service'
 
 async function getAllDishes() {
   try {
-    const res = await nami_api.get('/dish', {
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    })
+    const res = await nami_api.get('/dish')
     return res.data
   } catch (err: any) {
-    const message = err.response?.data?.message || err.message || 'Error al obtener usuarios'
+    const message = err.response?.data?.message || err.message || 'Error al obtener platos'
     throw new Error(message)
   }
 }
 
 async function getDishById(id: number | string) {
   try {
-    const res = await nami_api.get(`/dish/${id}`, {
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    })
+    const res = await nami_api.get(`/dish/${id}`)
     return res.data
   } catch (err: any) {
     const message = err.response?.data?.message || err.message || 'Error al obtener el plato'
