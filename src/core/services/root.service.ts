6fccc432372861f7ea41212f697c37db3d1fc6d0
Haui <luigfmp@gmail.com>
2025-11-04 05:31:54 -0500

    feat: ✨ Refactor dish and user services for improved API interaction; add session management methods

--- src/core/services/root.service.ts
+++ src/core/services/root.service.ts
@@ -2,7 +2,8 @@ import { API_URL } from '@/shared/config/constants'
 import axios from 'axios'
 
 const nami_api = axios.create({
-  baseURL: `${API_URL}/api/v1`
+  baseURL: `${API_URL}/api/v1`,
+  withCredentials: true
 })
 
 export default nami_api
