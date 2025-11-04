ce3c5bbd807c976113f4997e68a028241f59aa24
Haui <luigfmp@gmail.com>
2025-11-04 05:32:43 -0500

    feat: ✨ Add shopping cart and session management stores using Zustand for state management

---
+++ src/core/store/useSession.ts
@@ -0,0 +1,19 @@
+import { StateCreator, create } from 'zustand'
+import { persist } from 'zustand/middleware'
+
+interface Props {
+  user: any | null
+  setUser: (user: any) => void
+  clearUser: () => void
+}
+
+const state: StateCreator<Props> = set => ({
+  user: null,
+  setUser: user => set({ user }),
+  clearUser: () => set({ user: null })
+})
+
+// const useSessionStore = create(persist(state, { name: 'nami-user' }))
+const useSessionStore = create(state)
+
+export default useSessionStore
