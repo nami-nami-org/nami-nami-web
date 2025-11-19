2838adea6ea89b61bdee62de769dd9d076263888
Haui <luigfmp@gmail.com>
2025-11-19 07:29:21 -0500

    fix API URL error

--- src/shared/config/constants/index.ts
+++ src/shared/config/constants/index.ts
@@ -3,4 +3,4 @@ export const APP_ENV = process.env.NEXT_PUBLIC_APP_ENV
 
 export const IS_DEV = (APP_ENV || NODE_ENV) === 'development'
 
-export const API_URL = IS_DEV ? 'http://localhost:8080' : process.env.NEXT_PUBLIC_AUTH_URL
+export const API_URL = IS_DEV ? 'http://localhost:8080' : process.env.NEXT_PUBLIC_API_URL
