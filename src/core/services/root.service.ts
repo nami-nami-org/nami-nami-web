6486bcc5934b45a53bdcac02c05b83c63acfd7ea
Haui <luigfmp@gmail.com>
2025-11-19 08:29:23 -0500

    remove default vars

--- src/core/services/root.service.ts
+++ src/core/services/root.service.ts
@@ -1,7 +1,7 @@
 import { API_URL } from '@/shared/config/constants'
 import axios from 'axios'
 
-console.log('API_URLAPI_URLAPI_URLAPI_URLAPI_URLAPI_URLAPI_URLAPI_URLAPI_URLAPI_URL', API_URL)
+console.log('------------>', {API_URL})
 const nami_api = axios.create({
   baseURL: `${API_URL}/api/v1`,
   withCredentials: true
