bbac2f182bfdd2612fe43cb43c64ab54c73a66bc
Haui <luigfmp@gmail.com>
2025-11-19 07:29:21 -0500

    fix API URL error

--- src/core/services/root.service.ts
+++ src/core/services/root.service.ts
@@ -1,6 +1,7 @@
 import { API_URL } from '@/shared/config/constants'
 import axios from 'axios'
 
+console.log('API_URLAPI_URLAPI_URLAPI_URLAPI_URLAPI_URLAPI_URLAPI_URLAPI_URLAPI_URL', API_URL)
 const nami_api = axios.create({
   baseURL: `${API_URL}/api/v1`,
   withCredentials: true
