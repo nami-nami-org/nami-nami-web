a1e1a3ab61e647dc0c640a3b54a3a7ffb4a1e4d5
Jefferson <jgonzalezchaca@gmail.com>
2025-12-02 00:41:41 -0500

    fix: corregir clave de caché para obtener platos recomendados

--- src/core/query/dishes.query.tsx
+++ src/core/query/dishes.query.tsx
@@ -5,10 +5,10 @@ import { dishesService } from '../services/dishes.service'
 const getAllDishes = () => {
   const queryClient = useQueryClient()
 
-  const cached = queryClient.getQueryData(['get-all-dishes'])
+  const cached = queryClient.getQueryData(['get-all-dishes-recomended'])
 
   return useQuery({
-    queryKey: ['get-all-dishes'],
+    queryKey: ['get-all-dishes-recomended'],
     queryFn: dishesService.getAllDishes,
     staleTime: 1000 * 60 * 5,
     refetchOnWindowFocus: false,
